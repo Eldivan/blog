be9d5dfd6beb874b57e43b83ee00421916f3419e
Eldivan <eldivan.f1@gmail.com>
2020-03-02 07:46:35 -0300

    renderizando post individual

--- src/main/java/com/spring/blog/utils/DummyData.java
+++ src/main/java/com/spring/blog/utils/DummyData.java
@@ -25,14 +25,14 @@ public class DummyData {
 		
 		Post post1 =new Post();
 		post1.setAutor("Eddie");
-		post1.setTitulo("Java/Spring/Angular");
-		post1.setTexto("Vamos lá");
+		post1.setTitulo("Notícias Populares");
+		post1.setTexto("Startups em ritmo acelerado de crescimento precisam, regularmente, aumentar a equipe para dar conta das novas demandas. Algumas delas, como o iFood, chegam a ter mais de 70 vagas abertas. Abaixo, você pode conferir uma lista de 10 startups com posições a serem preenchidas.");
 		post1.setData(LocalDate.now());
 		
 		Post post2 = new Post();
 		post2.setAutor("Maico");
-		post2.setTitulo("Rails/Ruby/Angular");
-		post2.setTexto("Show");
+		post2.setTitulo("Notícias Necessárias");
+		post2.setTexto("Dezembro chegou, e muita gente gosta de fazer previsões para o ano novo que está batendo à porta. A consultoria Gartner não ficou de fora, e apontou as dez principais tendências tecnológicas para 2020–que vão levar disrupção e criar as maiores oportunidades no mercado pelos próximos cinco ou dez anos.");
 		post2.setData(LocalDate.now());
 		
 		postlist.add(post1);
